2da73c1c95cc852a136fdbfcbf7fd3ed0106362e
dj-kitty <david.johnson@malad.us>
2025-09-01 14:26:21 -0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,47 @@
+
+
+/**
+ * Hello
+ */
+public class Hello {
+
+        public static void main(String[] args) {
+            String x = "Hello World";
+            String dname = "David Johnson";
+            String davidage = "15";
+            String mari = "Mari";
+            int mariage = 18;
+            String fran = "Francisco (or Isaac)";
+            int franage = 18;
+            String drew = "Andrew";
+            int drewage = 25;
+            String jack = "Joshua (or Jack)";
+            int jackage = 22;
+            String mom = "Elizabeth";
+            int momage = 52;
+            String paul = "Paul";
+            int paulage = 14;
+            String nigh = "Nigh";
+            int nighage = 18;
+            String dad ="Brian";
+            int dadage = 54;
+
+            System.out.println(x);
+            System.out.println("My name is "+ dname + ", and I am " + davidage + " years old.");
+            System.out.println("My oldest brother is " + drew + " and he is " + drewage +".");
+            System.out.print("My second oldest brother is " + jack + ".");
+            System.out.println(" He is " + jackage + ".");
+            System.out.print("My oldest sister is " + mari + ".");
+            System.out.println(" She is " + mariage + ".");
+            System.out.print("My third oldest brother is " + fran + ".");
+            System.out.println(" He is " + franage + ".");
+            System.out.print("My second oldest sister is " + nigh + ".");
+            System.out.println(" She is " + nighage + ".");
+            System.out.print("My only younger sibling is my brother, " + paul + ".");
+            System.out.println(" He is " + paulage + ".");
+            System.out.print("My mother is "  + mom + ".");
+            System.out.println(" She is " + momage + " years old.");
+            System.out.print("My father is " + dad + ".");
+            System.out.println(" He is " + dadage + " years old.");
+        }
+}
